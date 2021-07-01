3aeeca802429385879240229c6fa9b74b0bb6d6c
sabinevidal <svidal@live.co.za>
2021-07-01 13:15:57 +0200

    RB101_2: rock paper scissors

---
+++ RB101/02_small_programs/rock_paper_scissors.rb
@@ -0,0 +1,58 @@
+# Walk through
+
+VALID_CHOICES = %w(rock paper scissors)
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt("You won!")
+  elsif win?(computer, player)
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample()
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing, goodbye!")
+
+=begin
+Things to consider
+
+- always be aware of the order or methods.
+  - ie display_results() above or below prompt()
+  - as prompt() is used in display_results()
+  - display_results() won't work if prompt() is after
+=end
